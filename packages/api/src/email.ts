0000e3ba22962fc2ae827f9b8c191c7d4e08c6cb
voidedstarlight <haii@voided.systems>
2026-07-15 22:17:17 +0100

    fix: include sender email in html content

--- packages/api/src/email.ts
+++ packages/api/src/email.ts
@@ -66,7 +66,7 @@ function attachEmailHandler(server: FastifyInstance) {
 
 			const { name, email, reason, message } = request.body as ContactRequestParams;
 
-			const html = `<!DOCTYPE html><body style="font-family:Helvetica,sans-serif;font-size:16px;margin:2em;padding:0"><h1 style="font-size:1.9em;font-weight:bold;margin-bottom:0.3em;margin-top:0;padding:0">Message Received on Contact Page</h1><p style="font-family:Helvetica,sans-serif;font-size:16px;margin:0;padding:0;">Sent by ${sanitise(name)} on ${new Date().toLocaleString("en-US", {timeZone: "US/Pacific"})} for ${sanitise(reason)}</p><pre style="white-space:pre-wrap;font-family:Helvetica,sans-serif;font-size:16px;border:1px solid #0a5d2a;border-radius:12px;margin-top:1.5em;padding:0.8em;margin-bottom:0;">${sanitise(message).replace("\n", "<br>")}</pre></body>`;
+			const html = `<!DOCTYPE html><body style="font-family:Helvetica,sans-serif;font-size:16px;margin:2em;padding:0"><h1 style="font-size:1.9em;font-weight:bold;margin-bottom:0.3em;margin-top:0;padding:0">Message Received on Contact Page</h1><p style="font-family:Helvetica,sans-serif;font-size:16px;margin:0;padding:0;">Sent by ${sanitise(name)} (${sanitise(email)}) on ${new Date().toLocaleString("en-US", { timeZone: "US/Pacific" })} for ${sanitise(reason)}</p><pre style="white-space:pre-wrap;font-family:Helvetica,sans-serif;font-size:16px;border:1px solid #0a5d2a;border-radius:12px;margin-top:1.5em;padding:0.8em;margin-bottom:0;">${sanitise(message).replace("\n", "<br>")}</pre></body>`;
 
 			const api_request = await fetch("https://api.brevo.com/v3/smtp/email", {
 				body: JSON.stringify({
